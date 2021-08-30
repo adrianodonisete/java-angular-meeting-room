346500f0402313c2afb53c0fa4d09c2611eeb27f
adriano d machado <adrianodonisete@gmail.com>
2021-08-29 23:15:39 -0300

    creating application

---
+++ backend-java/src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -0,0 +1,43 @@
+package com.digital.crud.saladereuniao.saladereuniao.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "meetingroom")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", startHour='" + startHour + '\'' +
+                ", endHour='" + endHour + '\'' +
+                '}';
+    }
+}
